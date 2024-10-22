2503263e88271ae5cd700019c0155d9497401fbc
Zihan <u7174903@anu.edu.au>
2024-10-22 14:34:27 +1100

    heroku

--- snap.py
+++ snap.py
@@ -15,7 +15,7 @@ def take_screenshot():
     # 上传图片到服务器
     files = {'file': ('screenshot.png', img_byte_arr, 'image/png')}
     try:
-        response = requests.post('https://snap-solver-460c670778fa.herokuapp.com/upload', files=files)
+        response = requests.post('http://localhost:3000/upload', files=files)
         if response.status_code == 200:
             print("Screenshot uploaded successfully")
         else:
